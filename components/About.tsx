7dabb40000c431c490113957960f2f2945238efc
James Casillas <jamesccasillas@gmail.com>
2023-08-21 17:59:16 -0700

    Fixed image spacing in About section

--- components/About.tsx
+++ components/About.tsx
@@ -5,35 +5,17 @@ type Props = {};
 
 export default function About({}: Props) {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-      className="flex flex-col relative h-screen font-inter text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
-    >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-light-slate text-2xl">
+    <div className="flex flex-col relative h-screen font-inter text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="top-24 uppercase tracking-[20px] text-light-slate text-2xl xl:absolute lg:absolute md:absolute sm:absolute">
         About
       </h3>
 
-      <motion.img
-        initial={{
-          x: -200,
-        }}
-        transition={{
-          duration: 0.8,
-        }}
-        whileInView={{ x: 0 }}
-        viewport={{ once: true }}
-        src="/aboutpic.jpg"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-64 h-64 rounded-full object-cover 
-        md:rounded-lg md:w-64 md:h-95 xl:w-[420px] xl:h-[520px] sm:rounded-lg"
-      />
-
-      <div className="space-y-10 px-0 md:px-10">
-        {/* <h4 className="text-off-white text-4xl font-semibold">
-          Background
-        </h4> */}
-        <p className="text-base text-justify">
+      <div className="px-0 md:px-10 py-10 flex flex-col xl:flex-row md:flex-row sm:flex-row">
+        <img
+          src="/icon.png"
+          className="relative object-cover w-50 xl:w-96 xl:h-96 xl:pt-0 md:pt-0 md:w-80 md:h-80 sm:pt-0 sm:w-64 sm:h-64"
+        />
+        <p className="text-base text-justify space-y-10 pt-10">
           My name's James, and I'm a full-stack developer from Phoenix, Arizona.
           I've been around computers for as long as I can remember and my desire
           to understand how they work led me to pursuing an education in
@@ -52,6 +34,6 @@ export default function About({}: Props) {
           world navigate their digital life at least a bit more easily.
         </p>
       </div>
-    </motion.div>
+    </div>
   );
 }
